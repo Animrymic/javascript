2c8c400382fee6fa43345d68cfab70cee8303f94
Animrymic <trojanovikdragan@gmail.com>
2026-02-09 20:49:00 +0100

    Add files via upload

---
+++ JSA05Wshop/scripts/script.js
@@ -0,0 +1,9 @@
+// 1. Select all the elements needed for the implementation
+// 2. Implement function to fetch data from API (provide fields what kind of data we need)
+// 3. Implement function that will display/hide spinner
+// 4. Implement function that will display data in Cards
+// 5. Implement function that will display data in Table
+// 6. Implement 5 event listeners for the 5 buttons
+// 6.1 Search, Reset, All from Europe, All neighbours MKD, Only MKD.
+// 7. Implement construction function with props only needed for the data to be displayed
+
